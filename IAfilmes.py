f6718fc479fe05621cffd4064b40f76f9e38d6a9
KlayvemG <klayvemguik@gmail.com>
2023-04-27 17:43:56 -0300

    ImplementationApi

--- IAfilmes.py
+++ IAfilmes.py
@@ -3,7 +3,7 @@ import json
 import random 
 
 # Insira sua chave de API aqui
-API_KEY = "sua_chave_de_api_aqui"
+API_KEY = "d4f37601d786141550b835c23ab6e2c6"
 
 # URL da API para filmes populares
 URL = f"https://api.themoviedb.org/3/movie/popular?api_key={API_KEY}"
@@ -11,8 +11,6 @@ URL = f"https://api.themoviedb.org/3/movie/popular?api_key={API_KEY}"
 # Faz uma solicitação GET para a API e armazena a resposta em um objeto JSON
 response = requests.get(URL)
 movies = json.loads(response.text)["results"]
-
-random = s
 
 # Seleciona um filme aleatório e exibe o título e a sinopse
 selected_movie = movies[random.randint(0, len(movies)-1)]
